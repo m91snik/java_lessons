c25ea53e4f42a3442b7b9310071db442366f2df6
artemeliseev <dingo550@yandex.ru>
2015-07-15 23:42:24 +0300

    Homework1

---
+++ artem.eliseev/homework1/src/Cat.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ната и Артем on 15.07.2015.
+ */
+public class Cat {
+    //кот ест
+    public String eat (String whatEat){
+        String phrase = "Eating " + whatEat;
+        return phrase;
+    };
+    public void sleep () {
+        System.out.println ("zz-z-z-z");
+    }
+}
