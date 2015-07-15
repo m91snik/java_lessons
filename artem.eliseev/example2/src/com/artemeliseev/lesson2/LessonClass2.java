22740c22cebaa8ba4358c7fa5bf36f1df6f67429
artemeliseev <dingo550@yandex.ru>
2015-07-15 21:38:50 +0300

    comment

---
+++ artem.eliseev/example2/src/com/artemeliseev/lesson2/LessonClass2.java
@@ -0,0 +1,7 @@
+package com.artemeliseev.lesson2;
+
+/**
+ * Created by Ната и Артем on 15.07.2015.
+ */
+public class LessonClass2 {
+}
---
+++ lessons/lesson_1/example2/src/newclass.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Anry on 14.07.2015.
+ */
+public class newclass {
+}
