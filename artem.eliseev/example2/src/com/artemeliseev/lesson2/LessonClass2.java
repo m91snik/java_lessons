338f7f5aa05405dfc6a79a2383825aabb4895751
artemeliseev <dingo550@yandex.ru>
2015-07-15 21:59:44 +0300

    commenttt

--- artem.eliseev/example2/src/com/artemeliseev/lesson2/LessonClass2.java
+++ artem.eliseev/example2/src/com/artemeliseev/lesson2/LessonClass2.java
@@ -4,4 +4,7 @@ package com.artemeliseev.lesson2;
  * Created by Ната и Артем on 15.07.2015.
  */
 public class LessonClass2 {
+    public static void main(String[] args) {
+    System.out.println("Hello  Any World!");
+}
 }
