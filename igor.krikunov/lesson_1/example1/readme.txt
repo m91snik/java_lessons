8b7e21e9810eccfdeadfca4cd7c22b406adb6d06
igor2i <igor2i.mail@gmail.com>
2015-07-16 10:37:55 +0400

    lesson 1

---
+++ igor.krikunov/lesson_1/example1/readme.txt
@@ -0,0 +1,3 @@
+1. Run "java Hello.java"
+2. Run "javac Hello.class" - Error: Could not find or load main class Hello.class
+3. Run "javac Hello"
---
+++ igor.krikunov/lesson_1/example2/src/com/igor2i/lesson1/Main.java
@@ -0,0 +1,8 @@
+package com.igor2i.lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello My World!");
+    }
+}
