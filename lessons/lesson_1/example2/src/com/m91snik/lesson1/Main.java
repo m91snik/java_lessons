1546647b56a53a0323b59441d10fb963e8b14810
m91snik <m91snik@mail.ru>
2015-07-17 10:17:23 +0400

    lesson1 changes

---
+++ lessons/lesson_1/example2/src/com/m91snik/lesson1/Main.java
@@ -0,0 +1,9 @@
+package com.m91snik.lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello My World!");
+        String str="dfgdfg";
+    }
+}
---
+++ lessons/lesson_1/example2/src/com/m91snik/lesson2/Main.java
@@ -0,0 +1,7 @@
+package com.m91snik.lesson2;
+
+/**
+ * Created by Valentin on 14.07.2015.
+ */
+public class Main {
+}
