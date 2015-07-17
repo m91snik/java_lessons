38da188f0207a4ebd8ea9aebd8754b961dbd7a0a
unknown <dmitriy_detkov@SSII-201-4.tke.local>
2015-07-17 10:07:49 +0300

    calc start commit

--- dmitriy.detkov/calc/src/Main.java
+++ dmitriy.detkov/calc/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String value = System.in.toString();
+        System.out.println(value);
     }
 }
