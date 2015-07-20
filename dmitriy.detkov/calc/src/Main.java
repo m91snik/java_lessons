8614f34091159ba1e9073a8575351b401ea52d7d
unknown <dmitriy_detkov@SSII-201-4.tke.local>
2015-07-20 11:10:41 +0300

    calc

--- dmitriy.detkov/calc/src/Main.java
+++ dmitriy.detkov/calc/src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // создаём объект класса Scanner
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the first argument (integer): ");
