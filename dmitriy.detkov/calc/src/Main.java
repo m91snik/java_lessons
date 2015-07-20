c3c609c66739f7cb08dc9c41c7990d985f0409a9
unknown <dmitriy_detkov@SSII-201-4.tke.local>
2015-07-20 11:01:18 +0300

    calc

--- dmitriy.detkov/calc/src/Main.java
+++ dmitriy.detkov/calc/src/Main.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String value = System.in.toString();
-        System.out.println(value);
+        // создаЄм объект класса Scanner
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("¬ведите первый аргумент (целое число): ");
+        int value1 = sc.nextInt();
+
+        System.out.print("¬ведите второй аргумент (целое число): ");
+        int value2 = sc.nextInt();
+
+        System.out.print("¬ведите символ операции (+ - * /): ");
+        char operator = sc.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                System.out.print(value1 + value2);
+                break;
+            case '-':
+                System.out.print(value1 - value2);
+                break;
+            case '*':
+                System.out.print(value1 * value2);
+                break;
+            case '/':
+                System.out.print(value1 / value2);
+                break;
+        }
     }
 }
