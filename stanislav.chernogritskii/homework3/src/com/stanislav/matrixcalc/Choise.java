d05e20f416c0d83b773ea7a9b9e7e5dbb65faf32
Stanislav Karasev <stas9005@gmail.com>
2015-07-23 00:59:24 +0300

    Matrix Calculator

---
+++ stanislav.chernogritskii/homework3/src/com/stanislav/matrixcalc/Choise.java
@@ -0,0 +1,15 @@
+package com.stanislav.matrixcalc;
+
+/**
+ * Created by stanislav on 23.07.15.
+ */
+
+public enum Choise {
+    MATRIX("1"), DIGIT("2");
+
+    String choise;
+
+    Choise (String choise) {
+        this.choise = choise;
+    }
+}
