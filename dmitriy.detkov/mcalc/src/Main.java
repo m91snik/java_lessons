898d27c66d8cf0a3a8339e33e759e7e0ad20a322
unknown <dmitriy_detkov@SSII-201-4.tke.local>
2015-07-23 08:04:49 +0300

    restore mcalc 2

--- dmitriy.detkov/mcalc/src/Main.java
+++ dmitriy.detkov/mcalc/src/Main.java
@@ -1,6 +1,17 @@
+import local.mcalc.Matrix;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        double[][] a ={{45,23,45},{56,67,78},{37,14,18},{67,67,67},{67,71,22}};
+        double[][] b ={{34,23,45},{12,67,78},{37,56,18},{45,67,67},{23,71,22}};
+
+        Matrix m1 = new Matrix(5,3);
+        m1.setMatrixArray(a);
+        Matrix m2 = new Matrix(5,3);
+        m2.setMatrixArray(b);
+
+        m1.print();
+        m2.print();
     }
 }
