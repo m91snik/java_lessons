ad383dc0ea59f36a1cd8a9c95241d8543da6d4b5
igor2i <igor2i.mail@gmail.com>
2015-07-24 18:21:33 +0400

    add lesson4

---
+++ igor.krikunov/lesson_4/lesson4/src/com/igor2i/lesson4/hashMap.java
@@ -0,0 +1,20 @@
+package com.igor2i.lesson4;
+
+import java.util.Objects;
+
+/**
+ * Created by igor2i on 23.07.15.
+ */
+public class hashMap {
+
+    static class Empty{
+        String hash;
+        Object date;
+    }
+
+    static private void site(){
+
+
+    }
+}
+
