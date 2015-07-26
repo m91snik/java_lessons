5c8b2c60bd65ae9a1f4b78e15df16a0e50d4f956
artemeliseev <dingo550@yandex.ru>
2015-07-26 16:08:22 +0300

    renaming

--- artem.eliseev/Homework3ArraysAndMatrix/src/com/Main.java
+++ artem.eliseev/Homework3ArraysAndMatrix/src/com/Main.java
@@ -1,88 +1,124 @@
 package com; /**
- * Created by Ќата и јртем on 22.07.2015.
+ * Created by Ќата и јртем on 23.07.2015.
+ * <p>
+ * ¬се продолжаетс€.
+ * <p>
+ * Ёто попытка сделать упражнение (операцию) дл€ неограниченного числа 2 мерных матриц. ¬се уперлось в то, что размеры основного
+ * хран€щего массива нужно задавать до циклов. Ёто возможно, но код разрастетс€ и на выполнение упражнени€ не хватит времени.
+ * Ѕыло потрачено около 3х часов, чтож, теперь это опыт.
  */
-/**
- * NOT FINISHED
- */
+
 import java.util.Arrays;
 import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
     public static void main(String... args) {
-        System.out.println("Hello.\nThis is matrix calculator. You can calculate 2 dimensional arrays.");
-        System.out.println("(ONLY SUM NOW!!!) Enter operation:");                                                                  //get operation
+        System.out.println("Hello.\nThis is matrix calculator. You can calculate 2 dimensional arrays. Max row and column number - 1000.");
+        System.out.println("Enter number of arrays:");                                                          //get number of arrays
+        Scanner scanner_quantity = new Scanner(System.in);
+        int quantity = scanner_quantity.nextInt();
+        System.out.println("Enter operation (+, -, /, *) :");                                                    //get operation
         Scanner scanner_oper = new Scanner(System.in);
-        char oper = scanner_oper.next().charAt(0);
-        System.out.println("1st Array. Enter row length:");                                                     //get array parameters
-        Scanner scanner_k1 = new Scanner(System.in);
-        int k1 = scanner_k1.nextInt();
-        System.out.println("1st Array. Enter column length:");
-        Scanner scanner_p1 = new Scanner(System.in);
-        int p1 = scanner_p1.nextInt();
-        System.out.println("Enter integer data. Type 'enter' after every variable:");                           //get array data
-        int[][] arr1 = new int[k1][p1];
-        Scanner scannerArr1 = new Scanner(System.in);
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr1[i].length; j++) {
-                arr1[i][j] = scannerArr1.nextInt();
-            }
+        char oper = '+';
+        try {
+            oper = (char) System.in.read();
+        } catch (IOException e) {
+            System.out.println("Input Error. Enter operation (+, -, /, *, 't' for transpose) :");
         }
-/**
- * 2nd array
- */
-        System.out.println("2nd Array. Enter row length:");                                                     //get array parameters
-        Scanner scanner_k2 = new Scanner(System.in);
-        int k2 = scanner_k2.nextInt();
-        System.out.println("2nd Array. Enter column length:");
-        Scanner scanner_p2 = new Scanner(System.in);
-        int p2 = scanner_p2.nextInt();
-        System.out.println("Enter integer data. Type 'enter' after every variable:");                           //get array data
-        int[][] arr2 = new int[k2][p2];
-        Scanner scannerArr2 = new Scanner(System.in);
-        for (int i2 = 0; i2 < arr2.length; i2++) {
-            for (int j2 = 0; j2 < arr2[i2].length; j2++) {
-                arr2[i2][j2] = scannerArr2.nextInt();
-            }
-        }
+        int[][][] arr = new int[1000][1000][quantity];
+        int kMax = 0, pMax = 0;
 
-//        System.out.println(Arrays.deepToString(arr1));
-        for (int x1 = 0; x1 < k1; x1++) {                                                                       //viewer
-            for (int y1 = 0; y1 < p1; y1++) {
-                System.out.print(arr1[x1][y1] + "\t");
+        for (int a = 0; a < quantity; a++) {
+            System.out.println((a + 1) + " Array. Enter row length:");                                           //get array parameters
+            Scanner scanner_k = new Scanner(System.in);
+            int k = scanner_k.nextInt();
+            if (k > kMax) {                                                                                      //return to viewer
+                kMax = k;
             }
+            System.out.println((a + 1) + " Array. Enter column length:");
+            Scanner scanner_p = new Scanner(System.in);
+            int p = scanner_p.nextInt();
+            if (p > pMax) {                                                                                      //return to viewer
+                pMax = p;
+            }
+            System.out.println("Enter integer data. Type 'enter' after every variable:");                        //get array data
+//            arr = new int[kMax][pMax][quantity];
+            Scanner scannerArr = new Scanner(System.in);
+            for (int i = 0; i < k; i++) {
+                for (int j = 0; j < p; j++) {
+                    arr[i][j][a] = scannerArr.nextInt();
+                }
+            }
+            System.out.println();
+            System.out.println("This array result:");
+            for (int xEvery = 0; xEvery < k; xEvery++) {
+                for (int yEvery = 0; yEvery < p; yEvery++) {
+                    System.out.print(arr[xEvery][yEvery][a] + "\t");
+                }
+                System.out.println();
+            }                                                                                                  //View every array
             System.out.println();
         }
         System.out.println();
-        for (int x2 = 0; x2 < k2; x2++) {
-            for (int y2 = 0; y2 < p2; y2++) {
-                System.out.print(arr2[x2][y2] + "\t");
+        System.out.println();
+        System.out.println("Your arrays:");
+        for (int z = 0; z < quantity; z++) {                                                                    //viewer
+            for (int x = 0; x < kMax; x++) {
+                for (int y = 0; y < pMax; y++) {
+                    System.out.print(arr[x][y][z] + "\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println();
+
+        }
+        int[][] arrFin = new int[kMax][pMax];
+        switch (oper) {
+            case '+':
+                for (int rowFin = 0; rowFin < kMax; rowFin++) {
+                    for (int columnFin = 0; columnFin < pMax; columnFin++) {
+                        for (int quantityFin = 0; quantityFin < quantity; quantityFin++) {
+                            arrFin[rowFin][columnFin] += arr[rowFin][columnFin][quantityFin];
+                        }
+                    }
+                }
+            case '-':
+                for (int rowFin = 0; rowFin < kMax; rowFin++) {
+                    for (int columnFin = 0; columnFin < pMax; columnFin++) {
+                        for (int quantityFin = 0; quantityFin < quantity; quantityFin++) {
+                            arrFin[rowFin][columnFin] -= arr[rowFin][columnFin][quantityFin];
+                        }
+                    }
+                }
+            case '*':
+                for (int rowFin = 0; rowFin < kMax; rowFin++) {
+                    for (int columnFin = 0; columnFin < pMax; columnFin++) {
+                        for (int quantityFin = 0; quantityFin < quantity; quantityFin++) {
+                            arrFin[rowFin][columnFin] *= arr[rowFin][columnFin][quantityFin];
+                        }
+                    }
+                }
+            case '/':
+                for (int rowFin = 0; rowFin < kMax; rowFin++) {
+                    for (int columnFin = 0; columnFin < pMax; columnFin++) {
+                        for (int quantityFin = 0; quantityFin < quantity; quantityFin++) {
+                            arrFin[rowFin][columnFin] /= arr[rowFin][columnFin][quantityFin];
+                        }
+                    }
+                }
+            case 't':
+
+        }
+//Result viewer
+        System.out.println("Operation result:");
+        for (int iResult = 0; iResult < kMax; iResult++) {
+            for (int jResult = 0; jResult < pMax; jResult++) {
+                System.out.print(arrFin[iResult][jResult] + "\t");
             }
             System.out.println();
         }
-        int kFin = 0, pFin = 0;
-        if (k1 >= k2) {
-            int kFin = k1;
-        } else {
-            int kFin = k2;
-        }
-        if (p1 >= p2) {
-            int pFin = p1;
-        } else {
-            int pFin = p2;
-        }
 
-        int[][] arrFin = new int[kFin][pFin];
-
-
-        switch (oper) {
-            case '+':
-                for (int iFin = 0; iFin < kFin;iFin++ ) {
-                    for (int jFin = 0;jFin<pFin ;pFin++ ) {
-                        arrFin [iFin][jFin]
-
-                    }
-                }
-
-        }
     }
 }
---
+++ artem.eliseev/Homework3ArraysAndMatrix/src/com/More_deep_develop.java
@@ -0,0 +1,7 @@
+package com;
+
+/**
+ * Created by Ната и Артем on 26.07.2015.
+ */
+public class More_deep_develop {
+}
