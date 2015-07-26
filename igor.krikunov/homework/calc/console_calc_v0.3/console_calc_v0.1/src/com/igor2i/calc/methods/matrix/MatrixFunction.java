5e14ee087977c123a93f747371056a10e5653628
igor2i <igor2i.mail@gmail.com>
2015-07-26 15:33:59 +0400

    edit matrix _ up v0.5

--- igor.krikunov/homework/calc/console_calc_v0.3/console_calc_v0.1/src/com/igor2i/calc/methods/matrix/MatrixFunction.java
+++ igor.krikunov/homework/calc/console_calc_v0.3/console_calc_v0.1/src/com/igor2i/calc/methods/matrix/MatrixFunction.java
@@ -1,5 +1,7 @@
 package com.igor2i.calc.methods.matrix;
 
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,7 +14,9 @@ enum MatrixFunction {
         @Override
         protected double[][] set(List<List<Double>> inArr1, List<List<Double>> inArr2) {
 
-            double[][] outDoub = new double[inArr1.size()][inArr1.get(0).size()];
+            int max[] = maxLenght(inArr1, inArr2);
+            double[][] outDoub = new double[max[0]][max[1]];
+
 
             for (int i = 0; i < inArr1.size(); i++) {
                 for (int n = 0; n < inArr1.get(i).size(); n++) {
@@ -25,7 +29,8 @@ enum MatrixFunction {
     MULT("*") {
         @Override
         protected double[][] set(List<List<Double>> inArr1, List<List<Double>> inArr2) {
-            double[][] outDoub = new double[inArr1.size()][inArr1.get(0).size()];
+            int max[] = maxLenght(inArr1, inArr2);
+            double[][] outDoub = new double[max[0]][max[1]];
 
             for (int i = 0; i < inArr1.size(); i++) {
                 for (int j = 0; j < inArr1.get(i).size(); j++) {
@@ -39,15 +44,29 @@ enum MatrixFunction {
             return outDoub;
         }
     },
-    INVO("^") {
+    SUBTR("-") {
+        @Override
+        protected double[][] set(List<List<Double>> inArr1, List<List<Double>> inArr2) {
 
+            int max[] = maxLenght(inArr1, inArr2);
+            double[][] outDoub = new double[max[0]][max[1]];
+
+
+            for (int i = 0; i < inArr1.size(); i++) {
+                for (int n = 0; n < inArr1.get(i).size(); n++) {
+                    outDoub[i][n] = inArr1.get(i).get(n) - inArr2.get(i).get(n);
+                }
+            }
+            return outDoub;
+        }
     },
-    TRANS("%"){
-        protected double[][] set(double [][] m){
-            double[][] temp = new double[m[0].length][m.length];
-            for (int i = 0; i < m.length; i++)
-                for (int j = 0; j < m[0].length; j++)
-                    temp[j][i] = m[i][j];
+    TRANS("%") {
+        @Override
+        protected double[][] set(List<List<Double>> inArr1) {
+            double[][] temp = new double[inArr1.get(0).size()][inArr1.size()];
+            for (int i = 0; i < inArr1.size(); i++)
+                for (int j = 0; j < inArr1.get(0).size(); j++)
+                    temp[j][i] = inArr1.get(i).get(j);
             return temp;
         }
 
@@ -67,9 +86,32 @@ enum MatrixFunction {
         double nul[][] = {{0}};
         return nul;
     }
-    protected double[][] set(List<List<Double>> inArr1) {
+
+    protected double[][] set(List<List<Double>> inArr1){
         double nul[][] = {{0}};
         return nul;
     }
 
+    protected int[] maxLenght(List<List<Double>> inArr1, List<List<Double>> inArr2) {
+        int max[] = new int[2];
+        if (inArr1.size() > inArr2.size()) {
+            max[0] = inArr1.size();
+        } else {
+            max[0] = inArr2.size();
+        }
+        max[1] = 0;
+        for (List<Double> sip : inArr1) {
+            if (sip.size() > max[1]) {
+                max[1] = sip.size();
+            }
+        }
+        for (List<Double> sip : inArr2) {
+            if (sip.size() > max[1]) {
+                max[1] = sip.size();
+            }
+        }
+
+        return max;
+
+    }
 }
