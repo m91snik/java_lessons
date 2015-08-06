296e36f4c59e559a7633fd834c6863388b5fa1e0
Stanislav Karasev <stas9005@gmail.com>
2015-08-06 11:20:19 +0300

    lesson 7

---
+++ stanislav.chernogritskii/lesson7/src/com/example/wildcards/Main.java
@@ -0,0 +1,12 @@
+package com.example.wildcards;
+
+/**
+ * Created by stanislav on 04.08.15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ stanislav.chernogritskii/lesson7/src/com/lesson/example/Comparator.java
@@ -0,0 +1,9 @@
+package com.lesson.example;
+
+/**
+ * Created by stanislav on 04.08.15.
+ */
+public interface Comparator<T> {
+
+    int compare(T o1, T o2);
+}
