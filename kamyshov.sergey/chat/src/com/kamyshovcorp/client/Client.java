3c3220d907a13c218b833a2a9b97a44cbf853bad
kamyshov.sergey <kamyshov.sergey@otr.ru>
2015-08-19 23:50:59 +0300

    Change Server impl to 2 threads

--- kamyshov.sergey/chat/src/com/kamyshovcorp/client/Client.java
+++ kamyshov.sergey/chat/src/com/kamyshovcorp/client/Client.java
@@ -1,25 +1,17 @@
 package com.kamyshovcorp.client;
 
-import java.io.IOException;
-import java.net.Socket;
-
 /**
  * Created by kamyshov.sergey on 17.08.15.
  */
 public class Client {
 
-    private static final String HOSTNAME = "127.0.0.1";
-    private static final int PORT = 1234;
-
     public static void main(String[] args) {
-        try (Socket socket = new Socket(HOSTNAME, PORT)) {
-            new Thread(new ClientReader(socket)).start();
-            new Thread(new ClientWriter(socket)).start();
+        try {
+            new Thread(new ClientReader()).start();
+            new Thread(new ClientWriter()).start();
 
-            // Ждем завершения других потоков, чтобы socket не закрылся
+            // Ждем завершения других потоков, чтобы поток Main не закрылся
             Thread.currentThread().join();
-        } catch (IOException e) {
-            e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- kamyshov.sergey/chat/src/com/kamyshovcorp/server/Server.java
+++ kamyshov.sergey/chat/src/com/kamyshovcorp/server/Server.java
@@ -1,28 +1,29 @@
 package com.kamyshovcorp.server;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+
+import com.kamyshovcorp.Message;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created by kamyshov.sergey on 17.08.15.
  */
 public class Server {
-
-    private static final int PORT = 1234;
+    private static BlockingQueue<Message> blockingQueue = new LinkedBlockingQueue<>();
 
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("Server started with port: " + PORT);
+        try {
+            new Thread(new ServerReader(blockingQueue)).start();
+            new Thread(new ServerWriter(blockingQueue)).start();
 
-            while (true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("Server accepted new client with address: " + socket.getLocalSocketAddress());
-
-                new Thread(new ServerThread(socket)).start();
-            }
-        } catch (IOException e) {
+            // Ждем завершения других потоков, чтобы поток Main не закрылся
+            Thread.currentThread().join();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 }
+
+
+
