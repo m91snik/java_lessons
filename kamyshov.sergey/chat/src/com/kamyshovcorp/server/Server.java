4685fb8e0b6db03875dd98b99da015852bcd1f38
kamyshov.sergey <kamyshov.sergey@otr.ru>
2015-08-22 00:32:21 +0300

    Добавил поддержку истории в чат

--- kamyshov.sergey/chat/src/com/kamyshovcorp/server/Server.java
+++ kamyshov.sergey/chat/src/com/kamyshovcorp/server/Server.java
@@ -11,6 +11,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
@@ -40,15 +41,17 @@ public class Server {
                             inputStream = new ObjectInputStream(socket.getInputStream());
                             message = (Message) inputStream.readObject();
                             clientInfo = message.getClientInfo();
+                            Date date = new Date();
                             if (MessageType.ENTRANCE == message.getMessageType()) {
                                 // Добавляем клиента в список пользователей
                                 users.put(clientInfo.getUserName(), new Pair<>(clientInfo.getHostName(), clientInfo.getClientPort()));
-                                System.out.println(String.format("Пользователь [%s, %s, %d] подключился к чату.", clientInfo.getUserName(), clientInfo.getHostName(), clientInfo.getClientPort()));
-                                continue;
+                                System.out.println(String.format("[%td/%tm/%ty %tT %tZ] Пользователь [%s, %s, %d] подключился к чату.",
+                                        date, date, date, date, date, clientInfo.getUserName(), clientInfo.getHostName(), clientInfo.getClientPort()));
                             } else if (MessageType.MESSAGE == message.getMessageType()) {
-                                System.out.println(String.format("Принятно сообщение от [%s, %s, %d]: ", clientInfo.getUserName(), clientInfo.getHostName(), clientInfo.getClientPort()) + message.getText());
-                                blockingQueue.add(message);
+                                System.out.println(String.format("[%td/%tm/%ty %tT %tZ] Принятно сообщение от [%s, %s, %d]: ",
+                                        date, date, date, date, date, clientInfo.getUserName(), clientInfo.getHostName(), clientInfo.getClientPort()) + message.getText());
                             }
+                            blockingQueue.add(message);
                         } catch (ClassNotFoundException e) {
                             e.printStackTrace();
                         }
@@ -65,7 +68,6 @@ public class Server {
             public void run() {
                 Message message = null;
                 ClientInfo clientInfo = null;
-                ObjectOutputStream outputStream;
                 while (true) {
                     try {
                         message = blockingQueue.take();
@@ -73,16 +75,23 @@ public class Server {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    // Если пользователь только вошел, то отправляем ему историю последних сообщений
+                    if (MessageType.ENTRANCE == message.getMessageType()) {
+                        String history = HistoryHandler.readHistory();
+                        Message historyMessage = new Message(MessageType.MESSAGE, history, clientInfo);
+                        ServerHandler.sendMessage(historyMessage, clientInfo.getHostName(), clientInfo.getClientPort());
+                        continue;
+                    } else if (MessageType.MESSAGE == message.getMessageType()) {
+                        Date date = new Date();
+                        // Записываем сообщение в историю перед отправкой всем пользователям
+                        HistoryHandler.writeHistory(String.format("[%td/%tm/%ty %tT %tZ, %s]: ", date, date, date, date, date,
+                                message.getClientInfo().getUserName()) + message.getText());
+                    }
                     for (Map.Entry<String, Pair<String, Integer>> user : users.entrySet()) {
-                        // Отсылаем всем кроме отправителя
+                        // Отсылаем сообщение всем кроме отправителя
                         if (!clientInfo.getUserName().equals(user.getKey())) {
                             Pair<String, Integer> userClientInfo = user.getValue();
-                            try (Socket socket = new Socket(userClientInfo.getKey(), userClientInfo.getValue())) {
-                                outputStream = new ObjectOutputStream(socket.getOutputStream());
-                                outputStream.writeObject(message);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+                            ServerHandler.sendMessage(message, userClientInfo.getKey(), userClientInfo.getValue());
                         }
                     }
                 }
