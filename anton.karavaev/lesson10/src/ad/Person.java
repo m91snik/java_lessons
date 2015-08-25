bd457949c1f5729747c9fe2ef4d7f0baae13aa82
AntonKaravaev <karavaevanton1993@gmail.com>
2015-08-25 19:28:42 +0300

    25.08

---
+++ anton.karavaev/lesson10/src/ad/Person.java
@@ -0,0 +1,32 @@
+package ad;
+
+/**
+ * Created by HP on 18.08.2015.
+ */
+public class Person {
+    private String name;
+    private int age;
+    private String password;
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
