076ec4691a7e3c1c0dedb0b9fee56e3c835c9b7d
Sasha <prog1@it-delta.ru>
2015-09-01 15:21:19 +0300

    Added maven, Server run with selector, Client run with socket channel, added test for blocking queue

---
+++ alexandr.nevyantsev/projects/SocketClient/src/main/java/com/makedonsky94/socket/WorkerReader.java
@@ -0,0 +1,45 @@
+package com.makedonsky94.socket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ * Created by Sasha on 18.08.2015.
+ */
+public class WorkerReader implements Runnable {
+    private ServerSocketChannel socketChannel;
+
+    public WorkerReader() throws IOException {
+        this.socketChannel = ServerSocketChannel.open();
+        this.socketChannel.bind(new InetSocketAddress("localhost", 4545));
+    }
+
+    @Override
+    public void run() {
+        SocketChannel inputSocketChannel = null;
+        while(true) {
+            try {
+                inputSocketChannel = this.socketChannel.accept();
+                ByteBuffer reader = ByteBuffer.allocate(256);
+                inputSocketChannel.read(reader);
+                System.out.println(new String(reader.array()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    inputSocketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ alexandr.nevyantsev/projects/SocketClient/src/main/java/com/makedonsky94/socket/WorkerWriter.java
@@ -0,0 +1,86 @@
+package com.makedonsky94.socket;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by Sasha on 18.08.2015.
+ */
+public class WorkerWriter implements Runnable {
+    private String color;
+    private String nick = null;
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public WorkerWriter() {
+        this.color = "\u001B[3" + (Math.abs(new Random().nextInt()) % 7 + 1) + "m";
+    }
+
+    @Override
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        SocketChannel socketChannel = null;
+        try {
+            socketChannel = SocketChannel.open(new InetSocketAddress("localhost", Main.DEFAULT_PORT));
+            socketChannel.configureBlocking(false);
+            //TODO get relative path to resources folder
+            String resourcesFolder = "./src/main/resources/";
+            Path nickFilePath = Paths.get(resourcesFolder + "nick");
+            if(Files.exists(nickFilePath)) {
+                nick = new String(Files.readAllBytes(nickFilePath));
+            }
+            if(nick == null) {
+                System.out.println("Input your nick name");
+                nick = scanner.next();
+                Files.write(nickFilePath, nick.getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+            }
+            ByteBuffer byteBuffer = ByteBuffer.allocate(256);
+            String newNick = "-nick " + nick;
+            //TODO send to server serializated object with config
+            byteBuffer.put(newNick.getBytes());
+            byteBuffer.flip();
+            while(byteBuffer.hasRemaining()) {
+                socketChannel.write(byteBuffer);
+            }
+            socketChannel.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        while ( true ) {
+            try {
+                String msg = scanner.nextLine();
+                socketChannel = SocketChannel.open(new InetSocketAddress("localhost", Main.DEFAULT_PORT));
+                StringBuilder stringBuilder = new StringBuilder();
+                //TODO set nick in server
+                stringBuilder.append(this.color);
+                stringBuilder.append(nick + ANSI_RESET + ": ");
+                stringBuilder.append(msg);
+                ByteBuffer byteBuffer = ByteBuffer.allocate(256);
+                byteBuffer.put(stringBuilder.toString().getBytes());
+                byteBuffer.flip();
+                while(byteBuffer.hasRemaining()) {
+                    socketChannel.write(byteBuffer);
+                }
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
