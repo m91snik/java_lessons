ecfed72a48b049617f09ca5b48ec7dde43ea14ac
artemeliseev <dingo550@yandex.ru>
2015-09-03 04:26:31 +0300

    Homework Chat (Stable version. works ok, most of the TODOes is done)

--- artem.eliseev/Homework_9.._chat/src/com/client/Client.java
+++ artem.eliseev/Homework_9.._chat/src/com/client/Client.java
@@ -12,29 +12,20 @@ public class Client {
     int portNumber;
     int inputClientPort;
 
-    public Client() {
-//        toDo Illegal Argument Exception
-        System.out.println("Enter host name (ip or www)  169.254.144.144 :");
-        hostName = Client.varGetter();
 
-        System.out.println("portNumber:");
-        portNumber = Integer.parseInt(Client.varGetter());
+    public Client(String hostName, int portNumber, int inputClientPort) {
+        this.hostName = hostName;
+        this.portNumber = portNumber;
+        this.inputClientPort = inputClientPort;
 
-        System.out.println("Enter your input port (Integer):");
-        inputClientPort = Integer.parseInt(Client.varGetter());
-        System.out.println();
+        //TO DO: it's better to create one Scanner for System.in and use nextLine to read each line: done
+        //TO DO: it makes sense to extract reading settings from System.in into Main class
+        // and pass already read settings as constructor argument to Client: Done
 
-        System.out.println("Enter messages:");
         ClientIn clientIn = new ClientIn(hostName, portNumber, inputClientPort);
         new Thread(clientIn).start();
 
         ClientOut clientOut = new ClientOut(inputClientPort);
         new Thread(clientOut).start();
-    }
-
-    @NotNull
-    public static String varGetter() {
-        Scanner inputClientPortScanner = new Scanner(System.in);
-        return inputClientPortScanner.nextLine().toString();
     }
 }
--- artem.eliseev/Homework_9.._chat_Server/src/com/server/InputChecker.java
+++ artem.eliseev/Homework_9.._chat_Server/src/com/server/InputChecker.java
@@ -8,6 +8,4 @@ public class InputChecker {
     public boolean sorter() {
         return true;
     }
-
-    ;
 }
