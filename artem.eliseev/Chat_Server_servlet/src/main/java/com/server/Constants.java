7f1dcb2a73bab3ba329c056b6e212df329da232c
artemeliseev <dingo550@yandex.ru>
2015-10-01 22:06:52 +0300

    Renaming. Chat - stable version. Working on TODOes; Client-jar, server-war,jar; Tombcat, rest - not finished

---
+++ artem.eliseev/Chat_Server_servlet/src/main/java/com/server/Constants.java
@@ -0,0 +1,14 @@
+package com.server;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Anry on 02.09.2015.
+ */
+@Component
+public class Constants {
+    public static final int SERVER_INPUT_PORT = 1025;
+
+    private Constants() {
+    }
+}
